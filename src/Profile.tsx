14bd5f71d96a80b02fd773d8dff44d5678de9c00
Supun Sudaraka <ssudaraka@gmail.com>
2024-09-26 16:09:12 +0530

    removed unwanted comments

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -10,8 +10,6 @@ import { supabase } from "./utils/supabaseClient";
 // });
 
 function Profile() {
-  const otpRef = useRef<HTMLInputElement>(null);
-
   const handleGetSession = async () => {
     const data = await supabase.auth.getSession();
     console.log(data);
@@ -27,117 +25,6 @@ function Profile() {
     console.log(data);
     console.log(error);
   };
-
-  const handlePhoneUpdate = async () => {
-    const { data, error } = await supabase.auth.updateUser({
-      phone: "94771867199",
-    });
-
-    if (error) {
-      console.log("error occurred while trying to update user's phone number");
-      console.error(error);
-      return;
-    }
-
-    console.log(data);
-  };
-
-  const handleOtpVerification = async () => {
-    const { data, error } = await supabase.auth.verifyOtp({
-      phone: "94771867199",
-      token: otpRef.current?.value || "",
-      type: "phone_change",
-    });
-
-    if (error) {
-      console.log("error occurred while trying to verify the otp");
-      console.error(error);
-      return;
-    }
-
-    console.log(data);
-  };
-  // const otpRef = useRef<HTMLInputElement>(null);
-
-  // async function handleUpdatePhone() {
-  //   const { data, error } = await supabase.auth.updateUser({
-  //     phone: "+94713522343",
-  //   });
-
-  //   if (error) {
-  //     console.log("error occurred while trying to update user's phone number");
-  //     console.error(error);
-  //     return;
-  //   }
-
-  //   console.log(data);
-  // }
-
-  // async function handleGetUser() {
-  //   const data = await supabase.auth.getUser();
-  //   console.log(data);
-  // }
-
-  // async function handleGetSession() {
-  //   const data = await supabase.auth.getSession();
-  //   console.log(data);
-  // }
-
-  // async function handleOtpVerification() {
-  //   if (otpRef.current) {
-  //     const { data, error } = await supabase.auth.verifyOtp({
-  //       phone: "+94713522343",
-  //       token: otpRef.current.value,
-  //       type: "phone_change",
-  //     });
-
-  //     if (error) {
-  //       console.log("error occurred while trying to verify the otp");
-  //       console.error(error);
-  //       return;
-  //     }
-
-  //     console.log(data);
-  //   }
-  // }
-
-  // return (
-  //   <>
-  //     <h1>Profile</h1>
-  //     <button
-  //       className="bg-blue-500 text-white py-2 px-4 rounded"
-  //       onClick={handleGetUser}
-  //     >
-  //       Get user
-  //     </button>
-  //     <button
-  //       className="bg-blue-500 text-white py-2 px-4 rounded"
-  //       onClick={handleGetSession}
-  //     >
-  //       Get session
-  //     </button>
-  //     <button
-  //       className="bg-blue-500 text-white py-2 px-4 rounded"
-  //       onClick={handleUpdatePhone}
-  //     >
-  //       Update phone
-  //     </button>
-  //     <br />
-  //     <input
-  //       className="shadow border"
-  //       type="text"
-  //       id="otp"
-  //       placeholder="Enter your OTP"
-  //       ref={otpRef}
-  //     />
-  //     <button
-  //       className="bg-blue-500 text-white py-2 px-4 rounded"
-  //       onClick={handleOtpVerification}
-  //     >
-  //       Verify OTP
-  //     </button>
-  //   </>
-  // );
 
   return (
     <div className="px-2 py-2">
